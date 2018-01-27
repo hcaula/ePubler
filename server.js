0eb08bfd2b9690d0771198ffe28cc74fd2b0e44b
hcaula <henrique.caula@hotmail.com>
2018-01-27 19:30:54 -0300

    Creates public directories in case they don't exist

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var http = require('http');
 var app = require('./config/express')();
 var connection = require('config').connection;
+var fs = require('fs');
 
 http.createServer(app).listen(app.get('port'), function() {
   var url = connection.protocol + "://";
   url += connection.host;
   url += ":" + connection.port;
 
+  createPublicDirectories();
+
   console.log("Server up at " + new Date());
   console.log('Express app listening on ' + url);
 });
+
+var createPublicDirectories = function() {
+  var directories = [
+    "./uploads/",
+    "./uploads/book",
+    "./uploads/cover",
+    "./unzips/"
+  ].forEach(function(dir){
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  });
+}
