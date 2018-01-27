a5daf27b81fac84c47d805acd0820884bd74f56b
hcaula <henrique.caula@hotmail.com>
2018-01-27 16:51:42 -0300

    Loads controllers

--- config/express.js
+++ config/express.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var load = require('express-load');
 
 module.exports = function() {
   var app = express();
@@ -6,6 +7,8 @@ module.exports = function() {
   app.set('port', 3000);
 
   app.use(express.static('./public'));
+
+  load('controllers', {cwd: 'app'}).into(app);
 
   return app;
 };
