ffe438296f0d38d3e66f1cea7461c8bda7f0b791
hcaula <henrique.caula@hotmail.com>
2018-01-28 02:26:19 -0300

    Some changes

--- app/lib/epubler.js
+++ app/lib/epubler.js
@@ -42,7 +42,25 @@ var getCover = function(req, res, next) {
     var copyPath = 'public/images/' + path.basename(book.file.filename, '.epub') + '.jpeg';
     fs.copyFileSync(unzipPath, copyPath);
     req.book.file.publicCover = copyPath.replace("public/images/", "");
-    req.book.file.unzipPath = unzipPath;
+    req.book.file.unzipDir = req.unzipDir;
+    next();
+  }
+}
+
+var getOpfFile = function(req, res, next) {
+  var opfDir = req.unzipDir + '/OEBPS/';
+  var opfFile;
+  fs.readdirSync(opfDir).forEach(function(file){
+    var extension = path.extname(file);
+    if(extension == ".opf") {
+      opfFile = file;
+      return;
+    }
+  });
+  if(!opfFile) {
+    next(new Error("This ePUB doesn't include a OPF file."));
+  } else {
+    req.book.file.opfFile = opfDir + opfFile;
     next();
   }
 }
@@ -51,19 +69,33 @@ exports.epubler = [
   getMetadata,
   epubToZip,
   unziper,
+  getOpfFile,
   getCover
 ]
 
 /*
  * GenePub middleware functions
 */
+var changeMetadata = function(req, res, next) {
+  var metadata = {
+    title: req.body.title,
+    description: req.body.description,
+    language: req.body.language,
+    creator: req.body.creator,
+    creatorFileAs: req.body.creatorFileAs,
+    date: req.body.date
+  }
 
-var getOpfFile = function(req, res, next) {
-  fs.readdirSync(testFolder).forEach(file => {
-    console.log(file);
-  })
+  var data = fs.readFileSync(req.body.opfFile, 'utf-8');
+  var old = '<dc:creator opf:file-as="Orwell, George" opf:role="aut">George Orwell</dc:creator>';
+  var newy = '<dc:creator opf:file-as="Orwell, George" opf:role="aut">New author test</dc:creator>';
+  var newValue = data.replace(old, newy);
+
+  fs.writeFileSync(req.body.opfFile, newValue, 'utf-8');
+
+  next();
 }
 
 exports.generateBook = [
-  getOpfFile
+  changeMetadata
 ]
